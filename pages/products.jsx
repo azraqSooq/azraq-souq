1a8e42812f4da90ee2d33d2172ea706eeb79f44d
qaisw96 <qaisw14@gmail.com>
2021-07-28 18:20:53 +0300

    product

---
+++ pages/products.jsx
@@ -0,0 +1,15 @@
+import Products from '../src/components/Products'
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+
+
+export default function products() {
+    return (
+        <div>
+            <Header />
+            <Products />
+            <Footer />
+            
+        </div>
+    )
+}
---
+++ src/components/Products.jsx
@@ -0,0 +1,50 @@
+import styles from '../../styles/component/products.module.scss'
+import Image from 'next/image'
+import {Button} from '@material-ui/core'
+
+import React from 'react'
+
+const Products = () => {
+    return (
+        <div className={styles.productsContainerParent}>
+
+            <div className={styles.productsContainer}>
+                {[1, 2, 3, 4, 5].map((el, ind) => {
+                    return (
+                        <>
+                            <div className={styles.product}>
+                                    <div className={styles.imageColumn}>
+                                        <Image src="/images/about.jpg" alt="" width={170} height={100} />
+                                    </div>
+                                    
+                                    <div className={styles.descriptionColumn}>
+                                        <p> أيبـــــــــــــد الجيــــل الثــامن</p>
+                                        <p> أيبـــــــــــــد الجيــــل الثــامن</p>
+                                        <div className={styles.productBtn}>
+                                            <Button className={styles.saveProduct} variant="contained">أضــــف  </Button>
+                                            <Button className={styles.callForProduct} variant="contained">اتصــــل</Button>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                        </>
+
+                    )
+
+
+                }) }
+                   
+                       
+                   
+
+            </div>
+
+
+
+            
+        </div>
+    )
+}
+
+export default Products 
